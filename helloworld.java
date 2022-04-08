45e719bd66710250c654cba7f4bd153ead0310be
[sami] <[samiseymen1010@gmail.com]>
2022-04-09 00:02:28 +0300

    Initial commit

--- helloworld.java
+++ helloworld.java
@@ -5,6 +5,10 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+
 
 	}
 
